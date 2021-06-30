554609c4f25914270ba205bdc9eb0fce2bc0ea79
ahohm <ahmed.hmayer@gmail.com>
2021-06-30 14:34:04 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from "./guards/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    //canActivate: [AuthGuard],
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(mod=>mod.DashboardModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(mod=>mod.AuthModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
